3c3642df14d97c5eabcf865a70870bd236ea81e0
TrickStealer <pritykinigor@gmail.com>
2024-08-26 15:23:47 +0200

    Make base for payment adding tests

--- cypress/e2e/payment_adding_positive.cy.js
+++ cypress/e2e/payment_adding_positive.cy.js
@@ -1,10 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Payment adding positive testing', () => {
+  beforeEach('Sign in', () => {
     cy.visit('https://fabrique:fabrique@finance.dev.fabrique.studio/')
-
     cy.get('[type="email"]').type('admin@admin.ad')
     cy.get('[type="password"]').type('admin')
     cy.get('.button__content').click()
+      cy.get('.pageLayout__actions .button__content').click()
+  })
 
+  it('Payment adding test 1', () => {
+    cy.get('[data-field-name="description"] .form-field').type('Test 1')
+  })
+
+  it('Payment adding test 2', () => {
+    cy.get('[data-field-name="description"] .form-field').type('Test 2')
   })
 })
