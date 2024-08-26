884c83b97c263debc649b74e65b6f6f690d30b0e
TrickStealer <pritykinigor@gmail.com>
2024-08-27 01:19:03 +0200

    Make almost all methods in class for payment filling

--- cypress/e2e/fill_payment.js
+++ cypress/e2e/fill_payment.js
@@ -1,15 +1,197 @@
 class FillPayment {
-  operationTypeIncome() {
-    return cy.get('[data-field-name="operation"] .radio-group__checkbox--first .checkbox__icon')
-        .should('have.class', 'checkbox__icon--checked')
-      }
-  operationTypeExpense(){
-    return cy.get('[data-field-name="operation"] .radio-group__checkbox--first')
+  // Тип операции "Доход/приход"
+  operationType_Income() {
+    cy.get('[data-field-name="operation"] .radio-group__checkbox--first')
+      .find('.checkbox__label')
+      .should('contain', 'Доход/приход')
+      .prev()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Тип операции "Расход"
+  operationType_Expense() {
+    cy.get('[data-field-name="operation"] .radio-group__checkbox--first')
       .next()
-      .find('.checkbox__icon')
+      .find('.checkbox__label')
+      .should('contain', 'Расход')
+      .prev()
       .click()
       .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Тип операции "Перевод средств"
+  operationType_Transfer() {
+    cy.get('[data-field-name="operation"] .radio-group__checkbox--last')
+      .find('.checkbox__label')
+      .should('contain', 'Перевод средств')
+      .prev()
+      .click()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Описание
+  description(text) {
+    cy.get('[data-field-name="description"] .form-field')
+      .type(text)
+  }
+
+  // Статус "Активен"
+  statusActive(active) {
+    cy.get('[data-field-name="statuses"] .radio-group__checkbox--first .checkbox__label')
+      .should('contain', 'Активен')
+      .prev()
+      .click()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Статус "Проверен"
+  statusChecked() {
+    cy.get('[data-field-name="statuses"] .radio-group__checkbox--last .checkbox__label')
+      .should('contain', 'Проверен')
+      .prev()
+      .click()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Сумма план
+  amountPlan(amount) {
+    cy.get('[data-field-name="amount_plan"] .input__input')
+      .type(amount)
+  }
+
+  // Сумма факт
+  amountFact(amount) {
+    cy.get('[data-field-name="amount_fact"] .input__input')
+      .type(amount)
+  }
+
+  // Статус оплаты "Не оплачен"
+  paymentStatus_NotPayed(){
+    cy.get('[data-field-name="status"] .radio-group__checkbox--first .checkbox__label')
+      .should('contain', 'Не оплачен')
+      .prev()
+      .click()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Статус оплаты "Оплачен"
+  paymentStatus_Payed(){
+    cy.get('[data-field-name="status"] .radio-group__checkbox--first')
+      .next()
+      .find('.checkbox__label')
+      .should('contain', 'Оплачен')
+      .prev()
+      .click()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Статус оплаты "Частично оплачен"
+  paymentStatus_PartlyPayed(){
+    cy.get('[data-field-name="status"] .radio-group__checkbox--last .checkbox__label')
+      .should('contain', 'Частично оплачен')
+      .prev()
+      .click()
+      .should('have.class', 'checkbox__icon--checked')
+  }
+
+  // Дата план (пока можно ставить только сегодняшнюю дату)
+  datePlan(){
+    cy.get('[data-field-name="date_plan"] .date__input')
+      .click()
+    cy.get('.dp-today')
+      .click()
+  }
+
+  // Дата факт (пока можно ставить только сегодняшнюю дату)
+  dateFact(){
+    cy.get('[data-field-name="date_fact"] .date__input')
+      .click()
+    cy.get('.dp-today')
+      .click()
+  }
+
+  // Источник
+  source(text){
+    cy.get('[data-field-name="source"] .multiselect__placeholder')
+      .click()
+    cy.get('[data-field-name="source"] .multiselect__input')
+      .type(text)
+      .type('{enter}')
+  }
+
+  // Источник уточнение
+  sourceAdditional(text){
+    cy.get('[data-field-name="source_additional_id"] .input__input')
+      .type(text)
+  }
+
+  // Статус документов. Варианты переменной text:
+    // Счет выставлен
+    // Счет отправлен
+    // Акт отправлен
+    // Акт подписан
+  documentsStatus(text){
+    if (text == 'Счет выставлен' ||
+        text == 'Счет отправлен' ||
+        text == 'Акт отправлен'  ||
+        text == 'Акт подписан') {
+          cy.get('[data-field-name="source_status"] .icon')
+            .click()
+          cy.get('[data-field-name="source_status"] .multiselect__option')
+            .contains(text)
+            .click()
     }
+    else {
+      assert(false, 'Incorrect input in FillPayment.documentsStatus(status)')
+    }
+  }
+
+  // Юридическое лицо
+  companyOwn(text){
+    cy.get('[data-field-name="company_own"] .multiselect__placeholder')
+      .click()
+    cy.get('[data-field-name="company_own"] .multiselect__input')
+      .type(text)
+      .type('{enter}')
+  }
+
+  // Контрагент
+  companyClient(text){
+    cy.get('[data-field-name="company_client"] .multiselect__placeholder')
+      .click()
+    cy.get('[data-field-name="company_client"] .multiselect__input')
+      .type(text)
+      .type('{enter}')
+  }
+
+  // Счет отправителя
+  accountSender(text){
+    cy.get('[data-field-name="account_sender"] .multiselect__placeholder')
+      .click()
+    cy.get('[data-field-name="account_sender"] .multiselect__input')
+      .type(text)
+      .type('{enter}')
+  }
+
+  // Счет получателя
+  accountRecipient(text){
+    cy.get('[data-field-name="account_recipient"] .multiselect__placeholder')
+      .click()
+    cy.get('[data-field-name="account_recipient"] .multiselect__input')
+      .type(text)
+      .type('{enter}')
+  }
+
+  // Тэги
+  // tags(text){
+  //   for (i in text) {
+  //     cy.get('[data-field-name="tags"] .multiselect__placeholder')
+  //       .click()
+  //     cy.get('[data-field-name="tags"] .multiselect__input')
+  //       .type(text[i])
+  //       .type('{enter}')
+  //   }
+  // }
 }
 
 export default FillPayment
--- cypress/e2e/payment_adding_positive.cy.js
+++ cypress/e2e/payment_adding_positive.cy.js
@@ -1,7 +1,7 @@
 import FillPayment from './fill_payment'
 
 describe('Payment adding positive testing', () => {
-  const fillPayment = new FillPayment()
+  const fill = new FillPayment()
 
   beforeEach('Sign in', () => {
     cy.visit('https://fabrique:fabrique@finance.dev.fabrique.studio/')
@@ -19,104 +19,27 @@ describe('Payment adding positive testing', () => {
   })
 
   it('Payment adding test 1', () => {
-    fillPayment.operationTypeIncome()
-    fillPayment.operationTypeExpense()
+    // fill.operationType_Income()                // Тип операции:	Доход/приход
+    // fill.description('Тест 1')                 // Описание:	Тест 1
+    // fill.statusActive()                        // Статус:	Активен
+    // fill.statusChecked()                       // Статус:	Проверен
+    // fill.amountPlan(2000)                      // Сумма план:	2000
+    // fill.amountFact(2000)                      // Сумма факт:	2000
+    // fill.paymentStatus_NotPayed()              // Статус оплаты:	Не оплачен
+    // fill.datePlan()                            // Дата план:	[Дата тестирования]
+    // fill.dateFact()                            // Дата факт:	[Дата тестирования]
+    // fill.source('Источник 1')                  // Источник:	Источник 1
+    // fill.sourceAdditional('Источник 1')        // Источник:	Источник 1
+    // fill.documentsStatus('Акт подписан')       // Статус документов:	Счёт выставлен
+    // fill.companyOwn('ИП Иванов Иван Иванович') // Юридическое лицо:	ИП Иванов Иван Иванович
+    // fill.companyClient('Контрагент 1')         // Контрагент:	Контрагент 1
+    // fill.accountSender('Счет отправителя 1')   // Счет отправителя:	Счет отправителя 1
+    // fill.accountRecipient('Счет получателя 1') // Счет получателя:	Счет получателя 1
+    // fill.tags(['Тэг 1', 'Тэг 2'])                       // Тэги	Тэг 1
 
 
-    // cy.get('[data-field-name="operation"] .radio-group__checkbox--first')
-    //   .next()
-    //   .children('.form-field__field')
-    //   .children('.checkbox--no-appearance')
-    //   .children('.checkbox__content')
-    //   .children('.checkbox__icon')
-    //   .click()
-
-      // cy.get('[data-field-name="operation"] .radio-group__checkbox--first')
-      //   .next()
-      //   .find('.checkbox__icon')
-      //   .click()
 
 
-    // // Тип операции	Доход/приход
-    // cy.get('[data-field-name="operation"] .radio-group__checkbox--first .checkbox__icon')
-    //   .should('have.class', 'checkbox__icon--checked')
-    //
-    // // Описание	Тест 1
-    // cy.get('[data-field-name="description"] .form-field').type('Тест 1')
-    //
-    // // Статус	Активен, проверен
-    // cy.get('[data-field-name="statuses"] .radio-group__checkbox--first .checkbox__icon')
-    //   .click()
-    // cy.get('[data-field-name="statuses"] .radio-group__checkbox--first .checkbox__icon')
-    //   .should('have.class', 'checkbox__icon--checked')
-    // cy.get('[data-field-name="statuses"] .radio-group__checkbox--last .checkbox__icon')
-    //   .click()
-    // cy.get('[data-field-name="statuses"] .radio-group__checkbox--last .checkbox__icon')
-    //    .should('have.class', 'checkbox__icon--checked')
-    //
-    // // Сумма план	2000
-    // cy.get('[data-field-name="amount_plan"] .input__input').type('2000')
-    //
-    // // Сумма факт	2000
-    // cy.get('[data-field-name="amount_fact"] .input__input').type('2000')
-    //
-    // // Статус оплаты	Не оплачен
-    // cy.get('[data-field-name="status"] .radio-group__checkbox--first .checkbox__icon')
-    //   .click()
-    // cy.get('[data-field-name="status"] .radio-group__checkbox--first .checkbox__icon')
-    //   .should('have.class', 'checkbox__icon--checked')
-    //
-    // // Дата план	[Дата тестирования]
-    // cy.get('[data-field-name="date_plan"] .date__input').click()
-    // cy.get('.dp-today').click()
-    //
-    // // Дата факт	[Дата тестирования]
-    // cy.get('[data-field-name="date_fact"] .date__input').click()
-    // cy.get('.dp-today').click()
-    //
-    // // Источник	Источник 1
-    // cy.get('[data-field-name="source"] .multiselect__placeholder').click()
-    // cy.get('[data-field-name="source"] .multiselect__input')
-    //   .type('Источник 1')
-    //   .type('{enter}')
-    //
-    // // Источник уточнение	Источник 1
-    // cy.get('[data-field-name="source_additional_id"] .input__input')
-    //   .type('Источник 1')
-    //
-    // // Статус документов	Счёт выставлен
-    // cy.get('[data-field-name="source_status"] .icon').click()
-    // cy.get('[data-field-name="source_status"] .multiselect__option')
-    //   .contains('Счет выставлен')
-    //   .click()
-    //
-    // // Юридическое лицо	ИП Иванов Иван Иванович
-    // cy.get('[data-field-name="company_own"] .multiselect__placeholder').click()
-    // cy.get('[data-field-name="company_own"] .multiselect__input')
-    //   .type('ИП Иванов Иван Иванович')
-    //   .type('{enter}')
-    //
-    // // Контрагент	Контрагент 1
-    // cy.get('[data-field-name="company_client"] .multiselect__placeholder')
-    //   .click()
-    // cy.get('[data-field-name="company_client"] .multiselect__input')
-    //   .type('Контрагент 1')
-    //   .type('{enter}')
-    //
-    // // Счет отправителя	Счет отправителя 1
-    // cy.get('[data-field-name="account_sender"] .multiselect__placeholder')
-    //   .click()
-    // cy.get('[data-field-name="account_sender"] .multiselect__input')
-    //   .type('Счет отправителя 1')
-    //   .type('{enter}')
-    //
-    // // Счет получателя	Счет получателя 1
-    // cy.get('[data-field-name="account_recipient"] .multiselect__placeholder')
-    //   .click()
-    // cy.get('[data-field-name="account_recipient"] .multiselect__input')
-    //   .type('Счет получателя 1')
-    //   .type('{enter}')
-    //
     // // Тэги	Тэг 1
     // cy.get('[data-field-name="tags"] .multiselect__placeholder')
     //   .click()
@@ -124,6 +47,11 @@ describe('Payment adding positive testing', () => {
     //   .type('Тэг 1')
     //   .type('{enter}')
     //
+
+
+
+
+    
     // // ID в банке	[Автоматически заполняется]
     // cy.get('[data-field-name="external_source_id"] .input__input')
     //   .should('be.disabled')
