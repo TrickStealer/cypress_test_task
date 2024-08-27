eb2d2e700c2be27abddd514f16231ccb87b8c50a
TrickStealer <pritykinigor@gmail.com>
2024-08-27 17:35:51 +0200

    Try to check text in payment's description

--- cypress/e2e/find_change_payment.js
+++ cypress/e2e/find_change_payment.js
@@ -4,6 +4,25 @@ class FindChangePayment {
     cy.get('.input__content').type(text).type('{enter}').type('{enter}')
     cy.contains(text).should('be.visible')
   }
+
+  // Открыть платёж
+  openPayment(text){
+    cy.contains(text).click()
+    cy.get('[data-field-name="description"] .form-field').invoke('text').should('include', 'Тест 999')
+
+
+    // cy.get('[data-field-name="description"] .form-field').focus()
+    // cy.focused().invoke('val').should('include', text);
+
+
+    // cy.get('[data-field-name="description"] .form-field').invoke('text').should('include', 'Тест 999')
+      // .should('have.value', text)
+
+    // cy.window().then((win) => {
+    //   // Проверяем, что текст присутствует в переменной JavaScript
+    //   expect(win.someJavaScriptVariable).to.include('Тест 999');
+    // });
+  }
 }
 
 export default FindChangePayment
--- cypress/e2e/payment_adding_positive.cy.js
+++ cypress/e2e/payment_adding_positive.cy.js
@@ -18,36 +18,37 @@ describe('Payment adding positive testing', () => {
   })
 
   it('Payment adding test 1', () => {
-    fill.openForm()                             // Открыть форму добавления платежа
-
-    fill.operationType_Income()                 // Тип операции:	Доход/приход
-    fill.description('Тест 999')                // Описание:	Тест 1
-    fill.statusActive()                         // Статус:	Активен
-    fill.statusChecked()                        // Статус:	Проверен
-
-    fill.amountPlan(2000)                       // Сумма план:	2000
-    fill.amountFact(2000)                       // Сумма факт:	2000
-    fill.paymentStatus_NotPayed()               // Статус оплаты:	Не оплачен
-
-    fill.datePlan()                             // Дата план:	[Дата тестирования]
-    fill.dateFact()                             // Дата факт:	[Дата тестирования]
-
-    fill.source('Источник 1')                   // Источник:	Источник 1
-    fill.sourceAdditional('Источник 1')         // Источник:	Источник 1
-    fill.documentsStatus('Акт подписан')        // Статус документов:	Счёт выставлен
-
-    fill.companyOwn('ИП Иванов Иван Иванович')  // Юридическое лицо:	ИП Иванов Иван Иванович
-    fill.companyClient('Контрагент 1')          // Контрагент:	Контрагент 1
-    fill.accountSender('Счет отправителя 1')    // Счет отправителя:	Счет отправителя 1
-    fill.accountRecipient('Счет получателя 1')  // Счет получателя:	Счет получателя 1
-
-    fill.tags(1)                                // Тэги:	Тэг 1
-    fill.bankID()                               // ID в банке	[Автоматически заполняется]
-
-    fill.addPaymentToTable()                    // Нажать кнопку "Добавить"
-    fill.closeForm()                            // Вернуться на страницу "Платежи"
+    // fill.openForm()                             // Открыть форму добавления платежа
+    //
+    // fill.operationType_Income()                 // Тип операции:	Доход/приход
+    // fill.description('Тест 999')                // Описание:	Тест 1
+    // fill.statusActive()                         // Статус:	Активен
+    // fill.statusChecked()                        // Статус:	Проверен
+    //
+    // fill.amountPlan(2000)                       // Сумма план:	2000
+    // fill.amountFact(2000)                       // Сумма факт:	2000
+    // fill.paymentStatus_NotPayed()               // Статус оплаты:	Не оплачен
+    //
+    // fill.datePlan()                             // Дата план:	[Дата тестирования]
+    // fill.dateFact()                             // Дата факт:	[Дата тестирования]
+    //
+    // fill.source('Источник 1')                   // Источник:	Источник 1
+    // fill.sourceAdditional('Источник 1')         // Источник:	Источник 1
+    // fill.documentsStatus('Акт подписан')        // Статус документов:	Счёт выставлен
+    //
+    // fill.companyOwn('ИП Иванов Иван Иванович')  // Юридическое лицо:	ИП Иванов Иван Иванович
+    // fill.companyClient('Контрагент 1')          // Контрагент:	Контрагент 1
+    // fill.accountSender('Счет отправителя 1')    // Счет отправителя:	Счет отправителя 1
+    // fill.accountRecipient('Счет получателя 1')  // Счет получателя:	Счет получателя 1
+    //
+    // fill.tags(1)                                // Тэги:	Тэг 1
+    // fill.bankID()                               // ID в банке	[Автоматически заполняется]
+    //
+    // fill.addPaymentToTable()                    // Нажать кнопку "Добавить"
+    // fill.closeForm()                            // Вернуться на страницу "Платежи"
 
     find.findPayment('Тест 999')  // Найти созданный платёж
+    find.openPayment('Тест 999')
 
     // cy.contains('Тест 1').click()
     // cy.get('[data-field-name="operation"] .radio-group__checkbox--first .checkbox__icon')
