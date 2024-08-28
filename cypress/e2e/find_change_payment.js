c47abb201811a5e501132bd54c65e063a8fd2902
TrickStealer <pritykinigor@gmail.com>
2024-08-28 16:35:42 +0200

    Add method to delete opened payment. Check it at full cycle. Fix bug of pyment finding method.

--- cypress/e2e/find_change_payment.js
+++ cypress/e2e/find_change_payment.js
@@ -2,15 +2,26 @@ class FindChangePayment {
   // Найти платёж
   findPayment(text){
     cy.get('.input__content').type(text).type('{enter}').type('{enter}')
-    cy.contains(text).should('be.visible')
+    cy.get('span').contains(text).should('be.visible')
   }
 
   // Открыть платёж и проврить, что это подходящий
   openPayment(text){
-    cy.contains(text).click()
+    cy.get('span').contains(text).parents('.table__record').click()
+    cy.get('.typography--type-heading')
+      .should('contain','Редактировать платёж')
     cy.get('[data-field-name="description"] .input__input')
       .invoke('val')
       .should('eq', text)
+  }
+
+  // Удалить открытый платёж
+  deletePayment(){
+    cy.get('.typography--type-heading')
+      .should('contain','Редактировать платёж')
+    cy.contains('Удалить').click()
+    cy.get('.swal2-confirm').click()
+    cy.get('.typography--type-heading').should('contain', 'Платежи')
   }
 }
 
